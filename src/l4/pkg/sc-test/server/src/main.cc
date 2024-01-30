4f59d94dba0a6a65c36e236e8cc19d56728befce
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2024-01-30 10:34:33 +0100

    brief example with SC attached to multiple threads

--- src/l4/pkg/sc-test/server/src/main.cc
+++ src/l4/pkg/sc-test/server/src/main.cc
@@ -12,11 +12,12 @@
 
 void *pthread_func(void *);
 
-void *pthread_func(void *)
+void *pthread_func(void *arg)
 {
+  char const *s = static_cast<char const *>(arg);
   for (;;)
   {
-    printf("Hello World!\n");
+    printf("Hello %s!\n", s);
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
@@ -31,14 +32,25 @@ int main(void)
   L4::Cap<L4::Factory> f = e->factory();
   L4::Cap<L4::Scheduler> s = e->scheduler();
 
-  pthread_t thread;
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
-  attr.create_flags |= PTHREAD_L4_ATTR_NO_START;
-  if (pthread_create(&thread, &attr, pthread_func, nullptr))
+  pthread_t thread1;
+  pthread_attr_t attr1;
+  pthread_attr_init(&attr1);
+  attr1.create_flags |= PTHREAD_L4_ATTR_NO_START;
+  void *arg1 = static_cast<void *>(const_cast<char *>("World1"));
+  if (pthread_create(&thread1, &attr1, pthread_func, arg1))
     L4Re::chksys(-L4_ENOSYS, "pthread create failure");
-  pthread_attr_destroy(&attr);
-  L4::Cap<L4::Thread> t(pthread_l4_cap(thread));
+  pthread_attr_destroy(&attr1);
+  L4::Cap<L4::Thread> t1(pthread_l4_cap(thread1));
+
+  pthread_t thread2;
+  pthread_attr_t attr2;
+  pthread_attr_init(&attr2);
+  attr2.create_flags |= PTHREAD_L4_ATTR_NO_START;
+  void *arg2 = static_cast<void *>(const_cast<char *>("World2"));
+  if (pthread_create(&thread2, &attr2, pthread_func, arg2))
+    L4Re::chksys(-L4_ENOSYS, "pthread create failure");
+  pthread_attr_destroy(&attr2);
+  L4::Cap<L4::Thread> t2(pthread_l4_cap(thread2));
 
   L4::Cap<L4::Timer_window_sc> sc;
   sc = L4Re::Util::cap_alloc.alloc<L4::Timer_window_sc>();
@@ -52,19 +64,23 @@ int main(void)
   L4Re::chksys(r, "sched_constraint factory create");
 
   s->set_prio(e->main_thread(), 254);
-  s->set_prio(t, 255);
+  s->set_prio(t1, 255);
+  s->set_prio(t2, 255);
 
-  s->attach_sc(t, sc);
+  s->attach_sc(t1, sc);
+  s->attach_sc(t2, sc);
 
   l4_sched_param_t sp = l4_sched_param(255, 0);
-  s->run_thread(t, sp);
+  s->run_thread(t1, sp);
+  s->run_thread(t2, sp);
 
   for(;;)
   {
     printf("%lld\n", l4_kip_clock(l4re_kip()));
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
-  //pthread_join(thread, nullptr);
+  //pthread_join(thread1, nullptr);
+  //pthread_join(thread2, nullptr);
 
   printf("Done\n");
 
